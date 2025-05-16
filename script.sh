eeccd98b694be76b5119f3e11267c16069bbdb47
AbuRider <dinarysf276@gmail.com>
2025-05-16 08:26:12 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -62,11 +62,11 @@ rm -rf hardware/mediatek
 
 rm -rf device/mediatek/sepolicy_vndr
 
-git clone https://github.com/VannTakashi/device_xiaomi_gale.git -b cartesian device/xiaomi/gale || { echo "Failed to clone device tree"; }
+git clone https://github.com/VannTakashi/device_xiaomi_gale.git -b lineage-22.2 device/xiaomi/earth || { echo "Failed to clone device tree"; }
 
-git clone https://github.com/VannTakashi/vendor_xiaomi_gale.git -b lineage-22.1 vendor/xiaomi/gale || { echo "Failed to clone vendor tree"; }
+git clone https://github.com/VannTakashi/vendor_xiaomi_gale.git -b lineage-22.2 vendor/xiaomi/earth || { echo "Failed to clone vendor tree"; }
 
-git clone https://github.com/VannTakashi/kernel_xiaomi_gale.git kernel/xiaomi/gale || { echo "Failed to clone kernel tree"; }
+git clone https://github.com/VannTakashi/kernel_xiaomi_gale.git kernel/xiaomi/earth || { echo "Failed to clone kernel tree"; }
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi || { echo "Failed to clone xiaomi stuffs"; }
 
@@ -78,7 +78,7 @@ git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git
 
 # Export Environment Variables
 echo "======= Exporting........ ======"
-export BUILD_USERNAME=takashiiprjkt
+export BUILD_USERNAME=priatampan
 export BUILD_HOSTNAME=crave
 export TZ=Asia/Jakarta
 export ALLOW_MISSING_DEPENDENCIES=true
@@ -95,4 +95,4 @@ echo "===================================="
 echo "  BRINGING TO HORIZON , STARTING BUILD.."
 echo "===================================="
 . build/envsetup.sh
-brunch gale
+brunch earth 
