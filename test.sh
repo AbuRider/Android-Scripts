62a57f87d8280faac88fd63554db4058dc09d19b
AbuRider <dinarysf276@gmail.com>
2025-06-04 18:35:03 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "cloning lineage priv keys"
-
+git clone https://github.com/ProjectMatrixx/android_vendor_lineage-priv_keys-template.git -b master vendor/lineage-priv/keys
+cd vendor/lineage-priv/keys
+./generate.sh
