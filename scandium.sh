3a7b56ede1fc0a09daa49c82913452351a8dc11c
AbuRider <dinarysf276@gmail.com>
2025-06-25 14:56:06 +0700

    Update scandium.sh

--- scandium.sh
+++ scandium.sh
@@ -1,1 +1,32 @@
+#!bin/bash
 
+# repo init
+repo init --depth=1 -u https://github.com/ScandiumOS-14/manifest -b 14 --git-lfs
+echo "repo init succses...."
+
+# sync
+/opt/crave/resync.sh || 
+echo "sync succsessfull...."
+
+# cloning trees bg
+
+
+
+
+
+
+
+# export
+export BUILD_USERNAME=
+export BUILD_HOSTNAME=
+export ALLOW_MISSING_DEPENDENCIES=
+echo "export done...."
+
+# start build enviroment
+source build/envsetup.sh
+
+# start build 
+echo "build will be start now...."
+. build/envsetup.sh
+lunch scandium_earth-ap2a-userdebug
+make bacon
